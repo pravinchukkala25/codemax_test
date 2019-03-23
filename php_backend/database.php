ec619583f08d0cd19b23a307e255385ed32d3090
Pravin Chukkala <pravin.chukkala25@gmail.com>
2019-03-23 13:03:34 +0530

    Database insert query created

--- php_backend/database.php
+++ php_backend/database.php
@@ -2,34 +2,56 @@
 
 /**
 * Database Class
-* =============================================================
-*/
-class Database{
-	private $host;
-	private $user;
-	private $pass;
-	private $db;
-	public $mysqli;
+	* =============================================================
+	*/
+	class Database{
+		private $host = 'localhost';
+		private $user = 'root';
+		private $pass = '';
+		private $db = 'inventory_db';
+		public $mysqli;
 
-	public function __construct() {
-		$this->db_connect();
-	}
+		// Check database connection =========
+		public function __construct() {
+			if(!isset($this->mysqli)) {
 
-	// DB Connection =============
-	private function db_connect(){
-		$this->host = 'localhost';
-		$this->user = 'root';
-		$this->pass = '';
-		$this->db = 'inventory_db';
+				$this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db);
 
-		$this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db);
+				if (!$this->mysqli) {
+					echo 'Cannot connect to database server';
+					exit;
+				}            
+			}    
 
-		if($mysqli->connect_errno) 
-		{
-			return "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-		} else {
 			return $this->mysqli;
 		}
+
+
+		// Insert Query ================
+		public function insert( $table , $data)
+		{
+			$row = array();
+			$column = array();
+			foreach ( $data as $col =>$value )
+			{
+				$row[] = $col;
+				$column[] = "'".$value."'";
+			}
+
+			$this->result = $this->mysqli->query("INSERT INTO ". $table ."(". implode(',' ,$row) .")
+				VALUES (". implode(',' , $column) .")");
+		}
+
+
+
 	}
 
-}
+
+	$db = new Database();
+	$db->insert(
+		'manufacturer_tb', 
+		array(
+			'm_name' => 'demoname',
+			'created_date' => date('Y-m-d H:i:s',strtotime('now'))
+		)
+	);
